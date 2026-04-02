f41cac280d0d7581cc03ed5c7944411ebcc3c15a
yqn1e23 <youquanneu3@gmail.com>
2026-04-02 09:45:14 +0800

    restructure dataset setting

--- Pipeline/Global/GallstoneDataSet.py
+++ Pipeline/Global/GallstoneDataSet.py
@@ -13,20 +13,13 @@ class GallstoneDataSet:
         self.file_path  = None
         self.target_col = None
 
-        self.splits = None
+        self.splits     = None
 
-        self.x_train        = None
-        self.x_train_scaled = None
-        self.y_train        = None
+        self.val_fold_split         = []
+        self.val_scaled_fold_split  = []
 
-        self.x_test         = None
-        self.x_test_scaled  = None
-        self.y_test         = None
-
-        self.main_scaler    = None
-
-        self.fold_split = []
-        self.scaled_fold_split = []
+        self.test_fold_split        = []
+        self.test_scaled_fold_split = []
 
     def apply_data(self):
         df = pd.read_csv(self.file_path)
@@ -43,47 +36,65 @@ class GallstoneDataSet:
         self.file_path = '../../Storage/Dataset/UCI_Gallstone_Dataset.csv'
         self.target_col = ['Gallstone Status']
         self.apply_data()
+    @staticmethod
+    def generate_k_fold_splits(base_x, base_y, n_splits):
 
-    def normal_data_split(self):
+        raw_folds       = []
+        scaled_folds    = []
 
-        x_train, x_test, y_train, y_test = train_test_split(
-            self.x,self.y,
-            test_size       = GlobalSetting.test_set_size,
-            random_state    = GlobalSetting.data_split_seed,
-            stratify        = self.y
-        )
+        skf = StratifiedKFold(n_splits  = n_splits,
+                              shuffle   = True,
+                              random_state = GlobalSetting.data_split_seed)
 
-        self.x_train = x_train.reset_index(drop=True)
-        self.y_train = y_train.reset_index(drop=True)
-
-        self.x_test = x_test.reset_index(drop=True)
-        self.y_test = y_test.reset_index(drop=True)
-
-        self.main_scaler = MinMaxScaler()
-        self.x_train_scaled = pd.DataFrame(self.main_scaler.fit_transform(x_train), columns=self.x.columns)
-        self.x_test_scaled  = pd.DataFrame(self.main_scaler.transform(x_test), columns=self.x.columns)
-
-    def cross_validate_split(self, n_splits = None):
-
-        self.splits = GlobalSetting.data_test_split if n_splits is None else n_splits
-        skf = StratifiedKFold(n_splits=self.splits,
-                              shuffle=True,
-                              random_state = GlobalSetting.data_split_seed
-                              )
-
-        self.fold_split = []
-        self.scaled_fold_split = []
-
-        for train_idx, test_idx in skf.split(self.x, self.y):
-
-            x_train_raw = self.x.iloc[train_idx].reset_index(drop=True)
-            x_test_raw  = self.x.iloc[test_idx] .reset_index(drop=True)
-            y_train = self.y.iloc[train_idx].reset_index(drop=True)
-            y_test  = self.y.iloc[test_idx] .reset_index(drop=True)
+        for train_idx, val_idx in skf.split(base_x, base_y):
+            x_train_raw = base_x.iloc[train_idx].reset_index(drop=True)
+            x_val_raw   = base_x.iloc[val_idx].reset_index(drop=True)
+            y_train_raw = base_y.iloc[train_idx].reset_index(drop=True)
+            y_val_raw   = base_y.iloc[val_idx].reset_index(drop=True)
 
             scaler = MinMaxScaler()
-            x_train_scaled  = pd.DataFrame(scaler.fit_transform(x_train_raw), columns=self.x.columns)
-            x_test_scaled   = pd.DataFrame(scaler.transform(x_test_raw), columns=self.x.columns)
+            x_train_scaled  = pd.DataFrame(scaler.fit_transform(x_train_raw), columns=base_x.columns)
+            x_val_scaled    = pd.DataFrame(scaler.transform(x_val_raw)      , columns=base_x.columns)
 
-            self.fold_split.append((x_train_raw, y_train, x_test_raw, y_test))
-            self.scaled_fold_split.append((x_train_scaled, y_train, x_test_scaled , y_test))
+            raw_folds.append((x_train_raw, y_train_raw, x_val_raw, y_val_raw))
+            scaled_folds.append((x_train_scaled, y_train_raw, x_val_scaled, y_val_raw))
+
+        return raw_folds, scaled_folds
+
+    @staticmethod
+    def generate_inner_holdout(base_x, base_y):
+        x_tr, x_val, y_tr, y_val = train_test_split(
+            base_x, base_y,
+            test_size       = 0.2,
+            random_state    = GlobalSetting.data_split_seed,
+            stratify        = base_y
+        )
+        x_tr    = x_tr.reset_index (drop=True)
+        x_val   = x_val.reset_index(drop=True)
+        y_tr    = y_tr.reset_index (drop=True)
+        y_val   = y_val.reset_index(drop=True)
+
+        scaler = MinMaxScaler()
+        x_tr_scaled     = pd.DataFrame(scaler.fit_transform(x_tr) , columns = base_x.columns)
+        x_val_scaled    = pd.DataFrame(scaler.transform(x_val)    , columns = base_x.columns)
+
+        return (x_tr, y_tr, x_val, y_val), (x_tr_scaled, y_tr, x_val_scaled, y_val)
+    def cv_test_split(self, n_splits = None):
+
+        self.val_fold_split = []
+        self.val_scaled_fold_split = []
+
+        self.test_fold_split = []
+        self.test_scaled_fold_split = []
+
+        self.splits = GlobalSetting.data_test_split if n_splits is None else n_splits
+        self.test_fold_split, self.test_scaled_fold_split = self.generate_k_fold_splits(
+            self.x, self.y, self.splits
+        )
+
+        for fold_idx in range(self.splits):
+            x_outer_tr_raw, y_outer_tr_raw, _, _ = self.test_fold_split[fold_idx]
+
+            raw_inner, scaled_inner = self.generate_inner_holdout(x_outer_tr_raw, y_outer_tr_raw)
+            self.val_fold_split.append(raw_inner)
+            self.val_scaled_fold_split.append(scaled_inner)
